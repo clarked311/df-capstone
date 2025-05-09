3d449a1840dc6e0dbd71e06f324273ba3b6824a6
Daniel Clarke <clarked311@hotmail.co.uk>
2025-05-09 14:08:10 +0100

    added people

--- etl/load/load.py
+++ etl/load/load.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
 try:
+    People = pd.read_csv('data/raw/People.csv')
     HoF = pd.read_csv('data/raw/HallOfFame.csv')
-
-    print(HoF)
 except Exception as e:
     print(f"An error occurred: {e}")
