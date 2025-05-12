40c254f1678f7231221c3bbfbfcdbc0b5b77d7b4
clarked311 <clarked311@hotmail.co.uk>
2025-05-12 21:34:54 +0100

    corrected function name

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 from load import load_HoF_data
+from transform import transform_HoF
 
 st.title("MLB")
 
@@ -13,4 +14,4 @@ HoF = datasets[1]
 People = datasets[0]
 Apps = datasets[2]
 
-trans_data = transform_data(HoF, People, Apps)
+trans_data = transform_HoF(HoF, People, Apps)
