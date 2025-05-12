99c016edf6c8db8d7db465a1d6d119198be72923
clarked311 <clarked311@hotmail.co.uk>
2025-05-12 23:26:32 +0100

    dropped bbrefID

--- etl/transform/transform.py
+++ etl/transform/transform.py
@@ -15,7 +15,7 @@ def transform_HoF(HoF, People, Apps):
 def column_manager(dataset):
     try:
         if 'deathCity' in dataset.columns:
-            dataset.drop(['ID', 'retroID'], axis=1, inplace=True)
+            dataset.drop(['ID', 'bbrefID', 'retroID'], axis=1, inplace=True)
         elif 'G_all' in dataset.columns:
             col_to_drop = ['G_all', 'GS', 'G_batting', 'G_defense', 'G_p', 'G_c', 'G_1b', 'G_2b', 'G_3b', 'G_ss', 'G_lf', 'G_cf', 'G_rf', 'G_of', 'G_dh', 'G_ph', 'G_pr']
             dataset.drop(col_to_drop, axis=1, inplace=True)
