ec599d86b46b24103023a511adf86d40116cb00a
clarked311 <clarked311@hotmail.co.uk>
2025-05-12 18:13:49 +0100

    started column_manager

--- etl/transform/transform.py
+++ etl/transform/transform.py
@@ -1,8 +1,19 @@
 import pandas as pd
 
-def transform_HoF(HoF,People):
+def transform_HoF(HoF, People):
     try:
-       
+        column_manager(HoF)
+        column_manager(People)
     except Exception as e:
         print(f"An error occurred: {e}")
     return trans_data
+
+def column_manager(dataset):
+    try:
+        if 'inducted' in dataset.columns:
+            dataset
+        elif 'deathCity' in dataset.columns:
+            dataset.drop('retroID', axis=1, inplace=True)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return dataset
