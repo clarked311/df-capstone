0cd4a4ab343e0e318915eb9b472d9557e8fb5361
clarked311 <clarked311@hotmail.co.uk>
2025-05-12 16:32:26 +0100

    loaded datasets to main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+from load.py import load_HoF_data
 
 st.title("MLB")
 
 st.write("MLB Stats")
+
+datasets = load_HoF_data()
+
+HoF = datasets[1]
+people = datasets[0]
