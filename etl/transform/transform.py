469ba9e496a85247071f1a271c7dfadac616c425
clarked311 <clarked311@hotmail.co.uk>
2025-05-12 20:56:59 +0100

    added apps to drops

--- etl/transform/transform.py
+++ etl/transform/transform.py
@@ -1,19 +1,23 @@
 import pandas as pd
 
-def transform_HoF(HoF, People):
+
+def transform_HoF(HoF, People, Apps):
     try:
-        column_manager(HoF)
         column_manager(People)
+        column_manager(Apps)
     except Exception as e:
         print(f"An error occurred: {e}")
     return trans_data
 
+
 def column_manager(dataset):
     try:
-        if 'inducted' in dataset.columns:
-            dataset
-        elif 'deathCity' in dataset.columns:
-            dataset.drop('retroID', axis=1, inplace=True)
+        if 'deathCity' in dataset.columns:
+            dataset.drop(['ID', 'retroID'], axis=1, inplace=True)
+        elif 'G_all' in dataset.columns:
+            dataset.drop(['G_all', 'GS', 'G_batting', 'G_defense', 'G_p', 'G_c', 'G_1b', 'G_2b', 'G_3b', 'G_ss', 'G_lf', 'G_cf', 'G_rf', 'G_of', 'G_dh', 'G_ph', 'G_pr'], axis=1, inplace=True)
+        else:
+            print('Dataset not found in manager')
     except Exception as e:
         print(f"An error occurred: {e}")
     return dataset
