57971599c4988d56225699d5dd7e5bde5b629f2e
clarked311 <clarked311@hotmail.co.uk>
2025-05-12 14:18:40 +0100

    turned into function

--- etl/load/load.py
+++ etl/load/load.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-try:
-    People = pd.read_csv('data/raw/People.csv')
-    HoF = pd.read_csv('data/raw/HallOfFame.csv')
-except Exception as e:
-    print(f"An error occurred: {e}")
+def load_HoF_data()
+    try:
+        People = pd.read_csv('data/raw/People.csv')
+        HoF = pd.read_csv('data/raw/HallOfFame.csv')
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return People,HoF
