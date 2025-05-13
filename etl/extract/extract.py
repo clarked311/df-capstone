8e0bddb5fa86513bbcc897dab43d1c662d367c9a
clarked311 <clarked311@hotmail.co.uk>
2025-05-13 14:10:27 +0100

    corrected encoding

--- etl/extract/extract.py
+++ etl/extract/extract.py
@@ -4,9 +4,9 @@ import pandas as pd
 def extract_HoF_data():
 
     try:
-        People = pd.read_csv('data/raw/People.csv', engine='python')
-        HoF = pd.read_csv('data/raw/HallOfFame.csv', engine='python')
-        Apps = pd.read_csv('data/raw/Appearances.csv', engine='python')
+        People = pd.read_csv('data/raw/People.csv', encoding='ISO-8859-15')
+        HoF = pd.read_csv('data/raw/HallOfFame.csv', encoding='ISO-8859-15')
+        Apps = pd.read_csv('data/raw/Appearances.csv', encoding='ISO-8859-15')
     except Exception as e:
         print(f"An error occurred: {e}")
     return People, HoF, Apps
