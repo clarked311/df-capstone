49639938d3f7ae63ed1f1afe5d24dfaafa275dba
clarked311 <clarked311@hotmail.co.uk>
2025-05-13 13:39:24 +0100

    made etl a function

--- src/main.py
+++ src/main.py
@@ -7,12 +7,17 @@ st.title("MLB")
 
 st.write("MLB Stats")
 
-datasets = extract_HoF_data()
 
-HoF = datasets[1]
-People = datasets[0]
-Apps = datasets[2]
+def etl_process():
+    try:
+        datasets = extract_HoF_data()
 
-trans_data = transform_HoF(HoF, People, Apps)
+        HoF = datasets[1]
+        People = datasets[0]
+        Apps = datasets[2]
 
-load_HoF(trans_data)
+        trans_data = transform_HoF(HoF, People, Apps)
+
+        load_HoF(trans_data)
+    except Exception as e:
+        print(f"An error occurred: {e}")
