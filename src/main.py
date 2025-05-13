1d5fe8e005baacbf0a88e58a4a498ccbb864a89c
clarked311 <clarked311@hotmail.co.uk>
2025-05-13 13:23:30 +0100

    added extraction to main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from etl.load.load import load_HoF_data
 from etl.transform.transform import transform_HoF
+from etl.extract.extract import extract_HoF
 
 st.title("MLB")
 
@@ -13,3 +14,5 @@ People = datasets[0]
 Apps = datasets[2]
 
 trans_data = transform_HoF(HoF, People, Apps)
+
+extract_HoF(trans_data)
