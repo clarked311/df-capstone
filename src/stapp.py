c632cee04f4c5d38010421dd2e8f05dac05a697a
clarked311 <clarked311@hotmail.co.uk>
2025-05-13 17:36:56 +0100

    implemented selection

--- src/stapp.py
+++ src/stapp.py
@@ -2,4 +2,20 @@ import pandas as pd
 import streamlit as st
 
 
-df = pd.read_csv('data/output/trans_HoF.csv')
+df = pd.read_csv('data/output/trans_HoF.csv', encoding='ISO-8859-1')
+
+
+def selector(df, player):
+    try:
+        return df[(df['playerID'] == player)]
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+st.title('MLB Hall of Fame')
+
+ID_list = ['a', 'b', 'c', 'd']
+
+player = st.selectbox('Select a Hall of Famer', ID_list)
+
+filtered_df = selector(df, player)
