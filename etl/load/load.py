3de177df8d1ed95ecb5298b17dda3fa6cbf38eea
clarked311 <clarked311@hotmail.co.uk>
2025-05-13 18:52:52 +0100

    added encoding

--- etl/load/load.py
+++ etl/load/load.py
@@ -4,7 +4,7 @@ from pathlib import Path
 def load_HoF(df):
     try:
         filepath = Path('data/output/trans_HoF.csv')
-        df.to_csv(filepath)
+        df.to_csv(filepath, encoding='ISO-8859-1')
     except Exception as e:
         print(f'An error occurred: {e}')
     return 'Extracted'
