381aea08e4178eb2ed880168481e6283b4a04d62
clarked311 <clarked311@hotmail.co.uk>
2025-05-13 17:42:45 +0100

    renamed to df

--- etl/load/load.py
+++ etl/load/load.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
 
-def load_HoF(dataset):
+def load_HoF(df):
     try:
         filepath = Path('data/output/trans_HoF.csv')
-        dataset.to_csv(filepath)
+        df.to_csv(filepath)
     except Exception as e:
         print(f'An error occurred: {e}')
     return 'Extracted'
