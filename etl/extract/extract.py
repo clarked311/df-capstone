17bc370680d6b14b16336816757fb6b200cf40ac
clarked311 <clarked311@hotmail.co.uk>
2025-05-13 13:22:08 +0100

    unmade directory

--- etl/extract/extract.py
+++ etl/extract/extract.py
@@ -4,7 +4,6 @@ from pathlib import Path
 def extract_HoF(dataset):
     try:
         filepath = Path('data/output/trans_HoF.csv')
-        filepath.parent.mkdir(parents=True, exist_ok=True)
         dataset.to_csv(filepath)
     except Exception as e:
         print(f'An error occurred: {e}')
