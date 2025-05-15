9c7ff7f3746abd8e1bf1a500a78a3a8e471eb29a
clarked311 <clarked311@hotmail.co.uk>
2025-05-15 03:00:56 +0100

    commented load

--- etl/load/load.py
+++ etl/load/load.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 
 
+# a function to load the transformed data in to the endpoint
 def load_HoF(HoF, People, Apps):
     try:
+        # the filepath variables tell pd where to write
         filepath = Path('data/output/trans_HoF.csv')
+        # .to_csv writes the file to the specified location.
+        # the encoding is there to avoid corruption.
         HoF.to_csv(filepath, encoding='ISO-8859-1')
         filepath = Path('data/output/trans_People.csv')
         People.to_csv(filepath, encoding='ISO-8859-1')
