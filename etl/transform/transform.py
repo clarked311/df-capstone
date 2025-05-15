e195aed534490aa8c380d2d9a5975ea86d6574a4
clarked311 <clarked311@hotmail.co.uk>
2025-05-15 03:12:03 +0100

    started commenting transform

--- etl/transform/transform.py
+++ etl/transform/transform.py
@@ -1,24 +1,34 @@
 import pandas as pd
 
 
+# the overarching function to transform the data
 def transform_HoF(HoF, People, Apps, Teams):
     try:
+        # sending the dfs with columns to be dropped to the manager
         People = column_manager(People)
         Apps = column_manager(Apps)
         Teams = column_manager(Teams)
+        # getting the ID list
         ID_list = list_IDs(HoF)
+        # paring down the dfs to those with required IDs
         HoF = row_manager(HoF, ID_list)
         People = row_manager(People, ID_list)
         Apps = row_manager(Apps, ID_list)
+        # added vote share to the HoF table
         HoF = vpc(HoF)
+        # merged teams on to the appearances table
+        # so the appearances have the name attached
         Apps = pd.merge(Apps, Teams, 'inner', on=['teamID', 'yearID'])
     except Exception as e:
         print(f"An error occurred in transform_HoF: {e}")
+    # returned the transformed dfs as a tuple
     return HoF, People, Apps
 
 
+# a function to drop columns that are unnecessary
 def column_manager(df):
     try:
+        # deathCity is only in people, so that identifies it
         if 'deathCity' in df.columns:
             df.drop(['ID', 'bbrefID', 'retroID'], axis=1, inplace=True)
         elif 'G_all' in df.columns:
